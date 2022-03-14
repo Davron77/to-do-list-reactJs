c60fc3abac7c382fb9226a6b47624347c453d9e5
Davron <davronbekq467@gmail.com>
2022-03-14 10:18:10 +0500

    add new item

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         </div>
       </form>
       <div className="grocery-container">
-        <List />
+        <List items={list} />
         <button className="clear-btn">Delete</button>
       </div>
     </section>
